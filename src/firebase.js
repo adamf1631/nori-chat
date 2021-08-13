1d5125eeb78ff2fb195608577d228cfa2cd457a4
Adam Falchetta <adamf1631@yahoo.com>
2021-08-13 14:33:05 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDf8dC58u0pa3IKN9m4z9KmvC27Tu6TdHg",
-    authDomain: "nori-chat-c08df.firebaseapp.com",
-    projectId: "nori-chat-c08df",
-    storageBucket: "nori-chat-c08df.appspot.com",
-    messagingSenderId: "683642402778",
-    appId: "1:683642402778:web:42d2c8ba687f29ec8172e3"
+  YOUR FIREBASE CONFIG OBJECT
 });
 
 export const firestore = app.firestore();
